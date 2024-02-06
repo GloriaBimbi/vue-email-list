787485a18fca50af4b734f3a79d69c19f392d454
GloriaBimbi <gloria.bimbi1@libero.it>
2024-02-06 16:12:32 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const { createApp } = Vue;
+const app = createApp({
+  data() {
+    return {
+      emails: [],
+    };
+  },
+
+  created() {
+    this.generateEmails();
+  },
+
+  methods: {
+    generateEmails() {
+      for (i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            console.log(response.data.response);
+            const email = response.data.response;
+            this.emails.push(email);
+            console.log(this.emails);
+          });
+      }
+    },
+  },
+  mounted() {},
+});
+
+app.mount("#app");
